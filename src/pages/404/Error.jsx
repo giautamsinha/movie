9c0a83351e15b5c4330a1f32dc25b137ac5dab81
giautamsinha <gautamsinha1122@gmail.com>
2023-07-31 09:02:32 +0530

    pagenotFound component changes

--- src/pages/404/Error.jsx
+++ src/pages/404/Error.jsx
@@ -1,8 +1,14 @@
 import "./style.scss"
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 const error = () => {
   return (
-    <div>error</div>
-  )
+    <div className="pageNotFound">
+        <ContentWrapper>
+            <span className="bigText">404</span>
+            <span className="smallText">Page not found!</span>
+        </ContentWrapper>
+    </div>
+);
 }
 
 export default error
